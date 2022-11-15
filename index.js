4b16872cbea65d44ec47d365ce60d3a315be3ea1
Damodharan <damodharan.kothandaraman@ncsc.go.bh>
2022-11-15 10:28:13 +0300

    env file added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http = require("http");
+require("dotenv").config();
 
 const hostname = "127.0.0.1";
-const port = 3002;
-
+const port = process.env.PORT || 3002;
 const server = http.createServer((req, res) => {
+  console.log("POST FROM ENV ===>", process.env.PORT);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello Node World welcome you ");
